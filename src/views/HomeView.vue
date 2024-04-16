41f7c03df0ff839dfed456ad3a7f2d07c0244c75
elmaccho <106253529+elmaccho@users.noreply.github.com>
2024-04-16 19:25:53 +0200

    Initial Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+
+  </main>
+</template>
