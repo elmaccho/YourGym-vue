160a0492c393c842a3e8b150fabe18cb1d7d905c
elmaccho <106253529+elmaccho@users.noreply.github.com>
2024-04-24 16:37:10 +0200

    css fix, footer layout change

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,21 +2,52 @@
     import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
     import { faPhone, faPaperPlane, faMap } from '@fortawesome/free-solid-svg-icons';
     import { faFacebookSquare, faInstagram, faYoutube, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+    import { useRouter, useRoute } from 'vue-router';
+
+  const router = useRouter();
+  const route = useRoute();
+
+  const isOnHomePage = () => {
+    return route.path === '/';
+  };
+
+  const scrollToSection = (sectionId) => {
+    if (!isOnHomePage()) {
+      router.push('/');
+    }
+
+    setTimeout(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 300); 
+  };
 </script>
+
 <template>
     <div class="footer-container">
         <div class="footer-content">
+            <div class="map-location">
+                <img src="@/assets/img/footer/das.jpg" alt="Lokalizacja siłowni">
+            </div>
             <div class="brand-info-wrapper">
-                <span class="d-flex flex-column align-items-center">
+                <span class="d-flex align-items-center gap-3 brand-wrapper">
                     <img src="@\assets\img\header\yourgymlogo-1.webp" class="mb-3 footer-brand-logo" alt="brand logo">
-                    <h1 class="footer-brand-name"><strong class="yg-main">YourGym</strong> Fitness Club</h1>
+                    <h1 class="footer-brand-name"><strong class="yg-main">YourGym</strong><br> Fitness Club</h1>
                 </span>
     
                 <div class="contact-box">
-                    <p>Kontakt:</p>
                     <span class="phone mb-4"><FontAwesomeIcon :icon="faPhone"/> +48 111-222-333</span>
                     <span class="email mb-4"><FontAwesomeIcon :icon="faPaperPlane"/> yourgym@gmail.com</span>
-                    <span class="address mb-4"><FontAwesomeIcon :icon="faMap"/> ul. Jakaśtam 64, 00-001, siedziba w Warszawie</span>
+                    <span class="address mb-4"><FontAwesomeIcon :icon="faMap"/> ul. Jakaśtam 64, 00-001,<br> siedziba w Warszawie</span>
+                </div>
+
+                <div class="section-box">
+                    <router-link class="footer-link mb-4" to="/kluby/">Nasze Kluby</router-link>
+                    <router-link class="footer-link mb-4" to="#about" @click="scrollToSection('about')">O nas</router-link>
+                    <router-link class="footer-link mb-4" to="#offert" @click="scrollToSection('offert')">Oferta</router-link>
+                    <router-link class="footer-link mb-4" to="#tickets" @click="scrollToSection('tickets')">Karnety</router-link>
                 </div>
     
                 <div class="social-wrapper">
@@ -25,9 +56,6 @@
                     <a href="#"><FontAwesomeIcon :icon="faYoutube" /></a>
                     <a href="#"><FontAwesomeIcon :icon="faXTwitter" /></a>
                 </div>
-            </div>
-            <div class="map-location">
-                <img src="@\assets\img\footer\image-4.webp" alt="Lokalizacja siłowni">
             </div>
         </div>
         <div class="copyright-bar">
@@ -39,21 +67,23 @@
     .footer-container{
         display: flex;
         flex-direction: column;
+        padding-top: 130px;
 
         background: rgb(0,0,0);
         background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(35,35,35,1) 100%);
     }
     .footer-content{
         display: flex;
+        flex-direction: column;
         justify-content: space-between;
         width: 100%;
     }
     .footer-brand-logo{
-        width: 350px;
+        width: 250px;
     }
     .brand-info-wrapper{
         display: flex;
-        flex-direction: column;
+        justify-content: space-around;
         align-items: center;
         width: 100%;
         padding: 35px 0 0;
@@ -68,7 +98,7 @@
         display: flex;
         flex-direction: column;
         color: white;
-        font-size: 21px;
+        font-size: 18px;
     }
     .phone, .email, .address{
         display: flex;
@@ -77,6 +107,7 @@
     }
     .social-wrapper{
         display: flex;
+        flex-direction: column;
         gap: 20px;
     }
     .social-wrapper > a {
@@ -105,6 +136,29 @@
         justify-content: center;
         text-align: center;
     }
+    .map-location{
+        width: 100%;
+    }
+    .map-location > img{
+        width: 100%;
+        height: 300px;
+        object-fit: cover;
+        object-position: center;
+    }
+    .section-box{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    }
+    .footer-link{
+        color: white;
+        text-decoration: none;
+        transition: 0.3s;
+        font-size: 18px;
+    }
+    .footer-link:hover{
+        color: #c94e06;
+    }
     @media screen and (max-width: 1385px) {
         .footer-content{
             flex-direction: column;
@@ -129,21 +183,43 @@
         .footer-brand-logo > img{
             width: 100%;
         }
-        .map-location{
-            width: 100%;
-        }
-        .map-location > img{
-            width: 100%;
-            height: 300px;
-            object-fit: cover;
+    }
+    @media screen and (max-width: 1000px) {
+        .footer-brand-logo{
+            width: 150px;
         }
     }
-    @media screen and (max-width: 667px) {
+    @media screen and (max-width: 800px) {
         .brand-info-wrapper{
             flex-direction: column;
+            gap: 25px;
         }
         .social-wrapper{
             flex-direction: row;
+        }
+        .section-box{
+            display: grid;
+            grid-template-columns: auto auto;
+            gap: 25px;
+        }
+        .footer-link{
+            width: max-content;
+        }
+        .map-location > img{
+            object-position: 25% 30px;
+        }
+    }
+    @media screen and (max-width: 444px) {
+        .brand-wrapper{
+            display: flex;
+            flex-direction: column;
+            gap: unset !important;
+        }
+        .footer-brand-logo{
+            width: 250px;
+        }
+        .map-location > img{
+            object-position: 30% 30px;
         }
     }
 </style>
