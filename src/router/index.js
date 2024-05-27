dffef3ec12e43ae8c057907da16698d762537cd3
elmaccho <106253529+elmaccho@users.noreply.github.com>
2024-05-27 17:34:00 +0200

    again routing fix

--- src/router/index.js
+++ src/router/index.js
@@ -32,9 +32,6 @@ import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
-  scrollBehavior(){
-    return { top: 0 }
-  },
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -207,7 +204,12 @@ const router = createRouter({
       name: 'register',
       component: RegisterView
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (to.name === 'login' || to.path.startsWith('/kluby') || to.path.startsWith('/oferta')) {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router
